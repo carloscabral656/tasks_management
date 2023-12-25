c0ad2600ca452fc9c0630b91a6b52d9564dd03bb
carloscabral656 <edu.carlos.cec@gmail.com>
2023-12-25 20:40:15 -0300

    Creating DTO and controller handler

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,6 +2,9 @@ import { Controller } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Get } from '@nestjs/common';
 import { Task } from './task.model';
+import { Post } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('/tasks')
 export class TasksController {
@@ -10,5 +13,10 @@ export class TasksController {
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto) {
+       return this.tasksService.createTask(createTaskDto); 
     }
 }
