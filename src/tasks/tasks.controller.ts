588b9d9b3583085397aa99ad6a7a799b25f05d70
carloscabral656 <edu.carlos.cec@gmail.com>
2023-12-25 19:08:41 -0300

    Model, controller and service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Get } from '@nestjs/common';
+import { Task } from './task.model';
 
-@Controller('tasks')
+@Controller('/tasks')
 export class TasksController {
-    
+    constructor(private tasksService: TasksService){}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
 }
